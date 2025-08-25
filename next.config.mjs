3be334cff9fc575fbc1ed0d8593b9c25599819f4
Ivan <glrzivan@gmail.com>
2025-08-25 13:05:12 -0700

    bugfixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  webpack: (config, { isServer }) => {
+    // This is the recommended way to enable polling for file watching
+    if (!isServer) {
+      config.watchOptions = {
+        poll: 1000, // Check for changes every second
+        aggregateTimeout: 200, // Add a small delay before rebuilding
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
